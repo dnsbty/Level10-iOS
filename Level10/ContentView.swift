cc75d2d2560919ef46fd698d72ce3929e01d9752
Dennis Beatty <dennis@dcbeatty.com>
2022-05-15 13:59:53 -0600

    Initial Commit

---
+++ Level10/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  Level10
+//
+//  Created by Dennis Beatty on 5/15/22.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ Level10/Level10App.swift
@@ -0,0 +1,17 @@
+//
+//  Level10App.swift
+//  Level10
+//
+//  Created by Dennis Beatty on 5/15/22.
+//
+
+import SwiftUI
+
+@main
+struct Level10App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
