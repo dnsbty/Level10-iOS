e02f2fe5720012f366bafb2bc5303509a00aa573
Dennis Beatty <dennis@dcbeatty.com>
2022-11-06 10:54:54 -0700

    fix LobbyView preview

--- Level10/Views/Screens/LobbyView.swift
+++ Level10/Views/Screens/LobbyView.swift
@@ -102,7 +102,29 @@ struct LobbyView: View {
 }
 
 struct LobbyView_Previews: PreviewProvider {
+    static var viewModel: GameViewModel {
+        let viewModel = GameViewModel()
+        viewModel.players = [
+            Player(name: "Dennis", id: "b95e86d7-82d5-4444-9322-2a7405f64fb8"),
+            Player(name: "Kira", id: "64d7f3b7-390e-4948-849e-a8745174db0e"),
+            Player(name: "Lily", id: "13c059c6-f4ad-4b05-87ca-2a61390f9042"),
+            Player(name: "Brett", id: "eb180f94-a899-4915-b2e8-57b8ea3e6103"),
+            Player(name: "Danny", id: "036a85e5-2b00-4cb7-8407-ca4b74ff4c7c"),
+            Player(name: "Megan", id: "cf34b6bf-b452-400a-a7f3-d5537d5a73b4")
+        ]
+        viewModel.connectedPlayers = [
+            "036a85e5-2b00-4cb7-8407-ca4b74ff4c7c",
+            "13c059c6-f4ad-4b05-87ca-2a61390f9042",
+            "b95e86d7-82d5-4444-9322-2a7405f64fb8",
+            "eb180f94-a899-4915-b2e8-57b8ea3e6103"
+        ]
+        viewModel.isCreator = true
+        viewModel.joinCode = "ABCD"
+        return viewModel
+    }
+    
     static var previews: some View {
         LobbyView()
+            .environmentObject(viewModel)
     }
 }
